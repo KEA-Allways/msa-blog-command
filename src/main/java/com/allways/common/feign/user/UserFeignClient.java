c6226b4c2b419a272d473056e2f57b67d776dd49
“mj” <75360915+minjun0707@users.noreply.github.com>
2023-11-30 13:52:58 +0900

    Jenkins Build Test

--- src/main/java/com/allways/common/feign/user/UserFeignClient.java
+++ src/main/java/com/allways/common/feign/user/UserFeignClient.java
@@ -4,10 +4,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="user-query" , url="${env.user-feign-url}")
+@FeignClient(name="user-query-service" , url="${env.user-feign-url}")
 public interface UserFeignClient {
 
-    @GetMapping("{userSeq}")
+    @GetMapping("/{userSeq}")
     UserFeignResponse queryUser(@PathVariable Long userSeq);
 
 }
