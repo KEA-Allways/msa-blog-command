d6f1a7f6b713a7b22b4748beeb9dfa52b827ee5c
DJ._.Dazzling <hers010801@gmail.com>
2023-11-30 14:39:06 +0900

    feign url 변수처리

--- src/main/java/com/allways/common/feign/fastApi/FastApiFeignClient.java
+++ src/main/java/com/allways/common/feign/fastApi/FastApiFeignClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.http.HttpStatus;
-@FeignClient(name = "fastApi", url = "http://localhost:8000")
+@FeignClient(name = "fastApi", url = "${env.file-feign-url}")
 public interface FastApiFeignClient {
     @PostMapping(value = "/api/feign/theme" )
     @ResponseStatus(HttpStatus.CREATED)
