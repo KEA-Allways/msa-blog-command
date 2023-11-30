6762abb677210dc668669883bcacbc7a33663769
“mj” <75360915+minjun0707@users.noreply.github.com>
2023-11-30 13:16:29 +0900

    Jenkins Build Test

---
+++ src/main/java/com/allways/common/feign/user/UserFeignClient.java
@@ -0,0 +1,13 @@
+package com.allways.common.feign.user;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name="user-query" , url="${env.user-feign-url}")
+public interface UserFeignClient {
+
+    @GetMapping("{userSeq}")
+    UserFeignResponse queryUser(@PathVariable Long userSeq);
+
+}
