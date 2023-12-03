83a1ad248ab17e8f11eb8aa6ff59a18ef2b36b76
“mj” <75360915+minjun0707@users.noreply.github.com>
2023-12-03 16:22:37 +0900

    build : elastic apm agent 추가

--- src/main/java/com/allways/common/feign/theme/ThemeFeignClient.java
+++ src/main/java/com/allways/common/feign/theme/ThemeFeignClient.java
@@ -5,8 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
-@FeignClient(name = "msa-blog-query", url = "${env.category-feign-url}" )
+// @FeignClient(name = "msa-blog-query", url = "${env.category-feign-url}" )
+@FeignClient(name = "blog-query-service")
 public interface ThemeFeignClient {
-    @GetMapping("/readThemeOrder/{userSeq}")
+    @GetMapping("api/theme/feign/readThemeOrder/{userSeq}")
     Long readLastThemeOrderByUserSeq(@PathVariable("userSeq") Long userSeq);
 }
