bb7ab571017962383c964c11a4fa2788bf955b7e
“mj” <75360915+minjun0707@users.noreply.github.com>
2023-12-03 16:27:23 +0900

    Squashed commit of the following:
    commit c390cb29870eaa13c17178e6071370f38de440de
    Author: “mj” <75360915+minjun0707@users.noreply.github.com>
    Date:   Sun Dec 3 16:25:18 2023 +0900
    
        build : elastic apm 설정 추가
    
    commit 83a1ad248ab17e8f11eb8aa6ff59a18ef2b36b76
    Author: “mj” <75360915+minjun0707@users.noreply.github.com>
    Date:   Sun Dec 3 16:22:37 2023 +0900
    
        build : elastic apm agent 추가
    
    commit d9ed50d557c4cfcb7538e4d07c795277bb546f3f
    Author: “mj” <75360915+minjun0707@users.noreply.github.com>
    Date:   Sun Dec 3 04:32:54 2023 +0900
    
        fix : db path 수정
    
    commit 6428f93b32254d97bffcc81bf3d2e5043c2c8dd8
    Author: “mj” <75360915+minjun0707@users.noreply.github.com>
    Date:   Fri Dec 1 17:53:44 2023 +0900
    
        Squashed commit of the following:
    
        commit e5c7d19a6d3141988412aa0ba29f3512b13bd11f
        Author: “mj” <75360915+minjun0707@users.noreply.github.com>
        Date:   Fri Dec 1 00:33:11 2023 +0900
    
            fix : feign url 수정
    
    commit 3477513272428be10c1bb7a7bd91f580a887d10d
    Merge: 23c1c1d 05515fd
    Author: “mj” <75360915+minjun0707@users.noreply.github.com>
    Date:   Fri Dec 1 17:53:30 2023 +0900
    
        Merge branch 'dev' of https://github.com/KEA-Allways/msa-blog-command into dev
    
    commit 05515fd8d830a966839b9f340ee9c99ff3811220
    Author: “sungjunkim” <“kis07150@naver.com”>
    Date:   Fri Dec 1 16:06:54 2023 +0900
    
        블로그 커맨드 수정
    
    commit 08b8905009925c1da2a086aa01cf88f71d15aefd
    Merge: 7f44dbb e864cd9
    Author: DJ._.Dazzling <hers010801@gmail.com>
    Date:   Fri Dec 1 01:08:05 2023 +0900
    
        Merge branch 'dev' of https://github.com/KEA-Allways/msa-blog-command into dev
    
    commit 7f44dbbf5e1a844d79a03fa331946e576b14e8e3
    Author: DJ._.Dazzling <hers010801@gmail.com>
    Date:   Fri Dec 1 01:07:46 2023 +0900
    
        게시글 수정 시 postSeq 반환 - 페이지 이동 시 필요

--- src/main/java/com/allways/common/feign/category/CategoryFeignClient.java
+++ src/main/java/com/allways/common/feign/category/CategoryFeignClient.java
@@ -4,10 +4,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "msa-category-query", url = "${env.category-feign-url}")
+// @FeignClient(name = "msa-category-query", url = "${env.category-feign-url}")
+@FeignClient(name = "blog-query-service")
 public interface CategoryFeignClient {
 
-    @GetMapping("/readCategoryOrder/{themeSeq}")
+    @GetMapping("api/category/feign/readCategoryOrder/{themeSeq}")
     Long readLastCategoryOrderByThemeSeq(@PathVariable("themeSeq") Long themeSeq);
 
 }
--- src/main/java/com/allways/common/feign/user/UserFeignClient.java
+++ src/main/java/com/allways/common/feign/user/UserFeignClient.java
@@ -4,10 +4,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="user-query-service" , url="${env.user-feign-url}")
+// @FeignClient(name="user-query-service" , url="${env.user-feign-url}")
+@FeignClient(name="user-query-service")
 public interface UserFeignClient {
 
-    @GetMapping("/{userSeq}")
+    @GetMapping("api/users/feign/{userSeq}")
     UserFeignResponse queryUser(@PathVariable Long userSeq);
 
 }
