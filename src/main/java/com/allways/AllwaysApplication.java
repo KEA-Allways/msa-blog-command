7d65350fb21c7e29f96ee333d8b1ef4606cf98df
“mj” <75360915+minjun0707@users.noreply.github.com>
2023-12-06 05:55:40 +0900

    Jenkins Build

--- src/main/java/com/allways/AllwaysApplication.java
+++ src/main/java/com/allways/AllwaysApplication.java
@@ -18,7 +18,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @EnableFeignClients
 public class AllwaysApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AllwaysApplication.class, args);
 	}
